cbc9a632c20f8aa39994cec12fba831269d09feb
Matthieu Coudron <mattator@gmail.com>
2019-09-10 23:39:44 +0900

    wip

--- papis/fzf.py
+++ papis/fzf.py
@@ -8,6 +8,8 @@ import papis.commands
 import papis.database
 import colorama
 import logging
+import subprocess
+import copy
 
 
 logger = logging.getLogger('fzf')
@@ -20,3 +22,11 @@ def main():
     documents = papis.database.get_all_query_string()
     # documents = papis.database.get().query(query)
     print("Documents", documents)
+    cmd = ['fzf', ]
+
+    # add a custom FZF_PREVIEW etc ?
+    # or wrap it
+    customEnv = copy.copy(os.environ)
+    # stdout=, 
+    subprocess.check_output(cmd, stdin="todo\nlala" , shell=True)
+
